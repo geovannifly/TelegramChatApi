739cc4d7c56792c68f7141c248081fa3be06c141
GeovanniFly <geovanni.fly@gmail.com>
2024-10-21 17:42:05 -0600

    Repository init

---
+++ src/main/java/com/umg/edu/gt/proyectofinal/TelegramChatApi/TelegramChatApiApplication.java
@@ -0,0 +1,13 @@
+package com.umg.edu.gt.proyectofinal.TelegramChatApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramChatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramChatApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umg/edu/gt/proyectofinal/TelegramChatApi/TelegramChatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umg.edu.gt.proyectofinal.TelegramChatApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramChatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
